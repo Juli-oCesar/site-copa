747bdf2674186117a443a60d936170a607981a13
Juli-oCesar <jcmelomarcolla@gmail.com>
2026-05-28 17:02:45 -0300

    Add files via upload

---
+++ trabalhoCopaDoMundo/trabalhoFinal/js/app.js
@@ -0,0 +1,105 @@
+// continentes
+
+function continenteAF() {
+    window.location.href = "./pages/continentes/africa/africa.html"
+
+}
+
+function continenteNA() {
+    window.location.href = "./pages/continentes/americaNorte/americaNorte.html"
+
+}
+
+function continenteSA() {
+    window.location.href = "./pages/continentes/americaSul/americaSul.html"
+
+}
+
+function continenteAS() {
+    window.location.href = "./pages/continentes/asia/asia.html"
+
+}
+
+function continenteEU() {
+    window.location.href = "./pages/continentes/europa/europa.html"
+
+}
+
+function continenteOC() {
+    window.location.href = "./pages/continentes/oceania/oceania.html"
+
+}
+
+// voltar para a pagina inicial
+
+function home() {
+    window.location.href = "/trabalhoFinal/index.html"
+
+}
+
+// paises
+
+function ale() {
+    window.location.href = "./pages/continentes/europa/paises/ale.html"
+}
+
+function aus() {
+    window.location.href = "./pages/continentes/europa/paises/aus.html"
+}
+
+function bel() {
+    window.location.href = "./pages/continentes/europa/paises/bel.html"
+}
+
+function bos() {
+    window.location.href = "./pages/continentes/europa/paises/bos.html"
+}
+
+function cro() {
+    window.location.href = "./pages/continentes/europa/paises/cro.html"
+}
+
+function esc() {
+    window.location.href = "./pages/continentes/europa/paises/esc.html"
+}
+
+function esp() {
+    window.location.href = "./pages/continentes/europa/paises/esp.html"
+}
+
+function fra() {
+    window.location.href = "./pages/continentes/europa/paises/fra.html"
+}
+
+function hol() {
+    window.location.href = "./pages/continentes/europa/paises/hol.html"
+}
+
+function ing() {
+    window.location.href = "./pages/continentes/europa/paises/ing.html"
+}
+
+function nor() {
+    window.location.href = "./pages/continentes/europa/paises/nor.html"
+}
+
+function por() {
+    window.location.href = "./pages/continentes/europa/paises/por.html"
+}
+
+function rep() {
+    window.location.href = "./pages/continentes/europa/paises/rep.html"
+}
+
+function sue() {
+    window.location.href = "./pages/continentes/europa/paises/sue.html"
+}
+
+function sui() {
+    window.location.href = "./pages/continentes/europa/paises/sui.html"
+}
+
+function tur() {
+    window.location.href = "./pages/continentes/europa/paises/tur.html"
+}
+
