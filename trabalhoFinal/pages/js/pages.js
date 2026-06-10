62b81008eeed8bd0f8f26c63913525a4abe1290c
Rodrigo <rodrigodaepic@gmail.com>
2026-06-10 17:12:27 -0300

    Página países atualizada

--- trabalhoFinal/pages/js/pages.js
+++ trabalhoFinal/pages/js/pages.js
@@ -165,3 +165,22 @@ if (btn && card) {
 }
 
 
+const btnConvocacao = document.getElementById("btnConvocacao");
+const cardConvocacao = document.getElementById("cardConvocacao");
+
+if(btnConvocacao && cardConvocacao){
+
+    btnConvocacao.addEventListener("click", () => {
+
+        if(cardConvocacao.style.display === "block"){
+            cardConvocacao.style.display = "none";
+            btnConvocacao.textContent = "Ver Convocados";
+        }
+        else{
+            cardConvocacao.style.display = "block";
+            btnConvocacao.textContent = "Ocultar Convocados";
+        }
+
+    });
+
+}
