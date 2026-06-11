dfbe32bc22f42a49d9fe1c9d921757b6da6a62db
Rodrigo <rodrigodaepic@gmail.com>
2026-06-11 16:26:27 -0300

    Só salvando

--- trabalhoFinal/pages/js/pages.js
+++ trabalhoFinal/pages/js/pages.js
@@ -184,6 +184,26 @@ if (btn && card) {
     });
 }
 
+const btnConvocacao = document.getElementById("btnConvocacao");
+const cardConvocacao = document.getElementById("cardConvocacao");
+
+if(btnConvocacao && cardConvocacao){
+
+    btnConvocacao.addEventListener("click", () => {
+
+        if(cardConvocacao.style.display === "block"){
+            cardConvocacao.style.display = "none";
+            btnConvocacao.textContent = "Ver Convocados";
+        }
+        else{
+            cardConvocacao.style.display = "block";
+            btnConvocacao.textContent = "Ocultar Convocados";
+        }
+
+    });
+
+}
+
 
 // carrossel 
 
@@ -236,22 +256,5 @@ if (btn && card) {
         if (e.key === 'ArrowLeft')  goTo(current - 1);
       });
     })();
-const btnConvocacao = document.getElementById("btnConvocacao");
-const cardConvocacao = document.getElementById("cardConvocacao");
 
-if(btnConvocacao && cardConvocacao){
 
-    btnConvocacao.addEventListener("click", () => {
-
-        if(cardConvocacao.style.display === "block"){
-            cardConvocacao.style.display = "none";
-            btnConvocacao.textContent = "Ver Convocados";
-        }
-        else{
-            cardConvocacao.style.display = "block";
-            btnConvocacao.textContent = "Ocultar Convocados";
-        }
-
-    });
-
-}
